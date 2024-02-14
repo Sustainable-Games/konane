28e299209dd7ae1257a7b05df7cb3434cee2eb58
Dan-TEALS <daniel.stormont@gmail.com>
2024-02-14 01:05:25 -0700

    Draw pits on board.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # konane
-The classic Hawaiian game of Konane created for js13kGames 2023.
+The classic Hawaiian game of Konane started for js13kGames 2023 and completed for the six monthiversary of the Maui fire.
+
+## About Konane
+Konane is a classic Hawaiian game played with pieces of colors (typically black lava rock and white coral) on a grid that can be square or rectangular. Common board sizes are 6 x 6 for a quick game to as large as 14 x 14; or 9×13, 14×17, or 13×20 for a rectangular board. The playing pieces are alternated to cover all of the spaces on the board in a checkerboard pattern.
+
+To begin play, players choose a color. Black always moves first. 
--- konane.js
+++ konane.js
@@ -3,12 +3,28 @@ let canvas = document.getElementById("gameBoard");
 let ctx = canvas.getContext("2d");
 
 // Get the board size from the user.
+let boardSize = 6;
 
 // Draw the board.
 ctx.fillStyle = "lightgrey";
-let board_width = canvas.width - 20;
-let board_height = canvas.height - 20;
-ctx.fillRect(10, 10, board_width, board_height);
+let boardWidth = canvas.width - 20;
+let boardHeight = canvas.height - 20;
+ctx.fillRect(10, 10, boardWidth, boardHeight);
+let cellSize = boardWidth / boardSize;
+for (let i = 0; i < boardSize; i++) {
+  for (let j = 0; j < boardSize; j++) {
+    ctx.beginPath();
+    const x = cellSize / 2 + i * cellSize + 10;
+    const y = cellSize / 2 + j * cellSize + 10;
+    const radius = cellSize / 2 - 20;
+    const startAngle = 0;
+    const endAngle = 2 * Math.PI;
+    ctx.arc(x, y, radius, startAngle, endAngle);
+    ctx.stroke();
+  }
+}
+
+// Initialize the board state.
 
 // Prompt the user for the piece color.
 
