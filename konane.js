9948670716aa7bdc5915c24fa17e963c0cf0ae09
Dan-TEALS <daniel.stormont@gmail.com>
2024-02-19 00:34:16 -0700

    Draw pieces on board. Fixes issue #2.

--- konane.js
+++ konane.js
@@ -6,22 +6,24 @@ let ctx = canvas.getContext("2d");
 let boardSize = 6;
 
 // Draw the board.
-ctx.fillStyle = "lightgrey";
-let boardWidth = canvas.width - 20;
-let boardHeight = canvas.height - 20;
-ctx.fillRect(10, 10, boardWidth, boardHeight);
-let cellSize = boardWidth / boardSize;
-for (let i = 0; i < boardSize; i++) {
-  for (let j = 0; j < boardSize; j++) {
-    ctx.beginPath();
-    const x = cellSize / 2 + i * cellSize + 10;
-    const y = cellSize / 2 + j * cellSize + 10;
-    const radius = cellSize / 2 - 20;
-    const startAngle = 0;
-    const endAngle = 2 * Math.PI;
-    ctx.arc(x, y, radius, startAngle, endAngle);
-    ctx.stroke();
-  }
+function drawBoard(size) {
+  ctx.fillStyle = "lightgrey";
+  let boardWidth = canvas.width - 20;
+  let boardHeight = canvas.height - 20;
+  ctx.fillRect(10, 10, boardWidth, boardHeight);
+  let cellSize = boardWidth / size;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      ctx.beginPath();
+      const x = cellSize / 2 + i * cellSize + 10;
+      const y = cellSize / 2 + j * cellSize + 10;
+      const radius = cellSize / 2 - 20;
+      const startAngle = 0;
+      const endAngle = 2 * Math.PI;
+      ctx.arc(x, y, radius, startAngle, endAngle);
+      ctx.stroke();
+    }
+  }  
 }
 
 // Initialize the board state.
@@ -40,7 +42,31 @@ console.table(boardState);
 // Prompt the user for the piece color.
 
 // Draw the pieces.
+function drawPieces(size, state) {
+  let boardWidth = canvas.width - 20;
+  let cellSize = boardWidth / size;
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      ctx.beginPath();
+      const x = cellSize / 2 + i * cellSize + 10;
+      const y = cellSize / 2 + j * cellSize + 10;
+      const radius = cellSize / 2 - 25;
+      const startAngle = 0;
+      const endAngle = 2 * Math.PI;
+      ctx.arc(x, y, radius, startAngle, endAngle);
+      if (state[i][j] === 0) {
+        ctx.fillStyle = "black";
+        ctx.fill();
+      } else {
+        ctx.fillStyle = "white";
+        ctx.fill();
+      }
+    }
+  }  
+}
 
 // Initial moves.
+drawBoard(boardSize);
+drawPieces(boardSize, boardState);
 
 // Game loop.
